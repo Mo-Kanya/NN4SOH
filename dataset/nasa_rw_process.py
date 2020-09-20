89388541510426fa10201c69b16c60c5043376e9
idlesilver <512845847@qq.com>
2020-09-20 22:08:02 +0800

    数据集、清洗、插值、噪声完成

--- dataset/nasa_rw_process.py
+++ dataset/nasa_rw_process.py
@@ -1,15 +1,18 @@
 # coding:UTF-8
 #%% 导入模块
+from numpy.lib.function_base import average
 import scipy.io as scio
 import numpy as np
 import matplotlib.pyplot as plt
-import time
+from scipy import interpolate
+import random
 
 # %% [markdown] 随机放电 RW
 # # 随机放电 RW
 
 #%% 导入文件 
 dataFile = r"./dataset/nasa/RW_Skewed_Low_40C_DataSet_2Post/data/Matlab/RW22.mat"
+# dataFile = r"./nasa/RW_Skewed_Low_40C_DataSet_2Post/data/Matlab/RW22.mat"
 data = scio.loadmat(dataFile)
 
 #%% 读取，预览数据
@@ -134,7 +137,8 @@ class RW_cycle:
         self.idx = idx
         self.charge = None
         self.discharge= []
-        self.merged_discharge= []
+        self.merged_discharge= None
+        self.capacity = 0
 
     @property
     def num_discharges(self):
@@ -173,6 +177,7 @@ class Cycle_record:
         self.ref_idx = 0
         self.num = 0
         self.capacity = 0
+        self.start_time = 0
         self.data = Data()
     def __repr__(self):
         return f'{self.num} infos, capacity={self.capacity:.3f}'
@@ -196,8 +201,8 @@ for i in range(data["data"]["step"][0, 0].shape[1]):
                     tuple(data["data"]["step"][0,0][attribute][0,i] for attribute in attributes)
                     )
         if cur == 'charge (after random walk discharge)':
-            cycle_records[ref_idx].data.next_RW_cycle()
             cycle_records[ref_idx].data.cur_cycle.charge = cur_data
+            cycle_records[ref_idx].data.next_RW_cycle()
         if cur == 'discharge (random walk)':
             cycle_records[ref_idx].data.cur_cycle.discharge.append(cur_data)
         cycle_records[ref_idx].data.all_useful.append(cur_data)
@@ -209,16 +214,18 @@ for i in range(data["data"]["step"][0, 0].shape[1]):
                             data["data"]["step"][0,0]['relativeTime'][0,i][0,:]/3600,
                             )
         cycle_records[ref_idx].capacity = capacity
+        cycle_records[ref_idx].start_time = data["data"]["step"][0,0]['time'][0,i][0,0]
+
         print(f'reference discharge at {i} capacity is {capacity:.5f}')
 
     # 如果出现 'rest post pulsed load'（大循环结束标志），则处理上一个大循环的信息，并重置大循环记录 
     if cur == 'rest post pulsed load':
         # 打印当前大循环记录的信息
         cycle_records[ref_idx].ref_idx = ref_idx
-        print('='*20+f"     {cycle_records[ref_idx].ref_idx} "+'='*20)
-        print(f"num is      {cycle_records[ref_idx].num}")
-        print(f"capacity is {cycle_records[ref_idx].capacity:.5f}")
-        print(f"data is     {cycle_records[ref_idx].data}")
+        print('='*20+f" cycle {cycle_records[ref_idx].ref_idx} "+'='*20)
+        print(f"num:      {cycle_records[ref_idx].num}")
+        print(f"capacity: {cycle_records[ref_idx].capacity:.5f}")
+        print(f"data:     {cycle_records[ref_idx].data}")
         # 重置大循环记录
         ref_idx += 1    # 当前大循环数
 
@@ -259,11 +266,111 @@ for ref_idx in range(len(cycle_records)):
         print(f"{e} at ref_idx:{ref_idx}")
 
 
-
+#%%
 # 填补SOH空缺值（插值）
 
+# ref中测定的capacity和对应时间
+t_p=[]
+capacity_p=[]
+for i in range(len(cycle_records)):
+    if cycle_records[i].capacity is not 0:
+        t_p.append(cycle_records[i].start_time)
+        capacity_p.append(cycle_records[i].capacity)
 
+# 需要插值的点，即RW_cycle对应的时间，以放电结束充电开始的时间点为准
+t = []
+for ref_idx in range(len(cycle_records)):
+    for idx in range(cycle_records[ref_idx].data.num_RW_cycles):
+        t.append(
+            cycle_records[ref_idx].data.RW_cycles[idx].charge[0,0]
+        )
 
+# 插值
+# f = interpolate.interp1d(t_p,capacity_p,kind='quadratic')
+f = interpolate.CubicSpline(t_p,capacity_p,bc_type="natural",extrapolate=True)
+capacity = f(t)
+
+# # 显示结果
+# plt.plot(t,capacity,'r')
+# plt.plot(t_p,capacity_p,'b')
+# plt.show()
+
+# 把对应的capacity放对应RWcycle
+counter = 0
+for ref_idx in range(len(cycle_records)):
+    for idx in range(cycle_records[ref_idx].data.num_RW_cycles):
+        cycle_records[ref_idx].data.RW_cycles[idx].capacity = capacity[counter]
+        counter += 1
+
+# 检测放回结果
+temp_t = []
+temp_c = []
+for ref_idx in range(len(cycle_records)):
+    for idx in range(cycle_records[ref_idx].data.num_RW_cycles):
+        temp_c.append(cycle_records[ref_idx].data.RW_cycles[idx].capacity)
+        temp_t.append(cycle_records[ref_idx].data.RW_cycles[idx].charge[0,0])
+plt.plot(t_p,capacity_p,'b')
+plt.plot(t,capacity,'r')
+plt.plot(temp_t,temp_c,'g')
+plt.show()
+
+#%% 整合所有的RWcycle数据
+all_rw_cycles = []
+for ref_idx in range(len(cycle_records)):
+    for idx in range(cycle_records[ref_idx].data.num_RW_cycles):
+        all_rw_cycles.append(cycle_records[ref_idx].data.RW_cycles[idx])
+
+#%% 添加随机噪声，偏移
+def add_noise(array,noise_percent=0.02):
+    time = array[0:2,:]
+    result = np.zeros_like(array,dtype='float64')
+    for i in range(array.shape[0]):
+        mean = array[i,:].mean()
+        noise = np.random.randn(array.shape[1])*mean*noise_percent
+        noise = noise - np.mean(noise)
+        result[i,:] = array[i,:]+noise
+    result[0:2,:] = time
+    return result
+
+def add_bias(array,
+             voltage_bias,
+             current_bias,
+             temperature_bias):
+    time = array[0:2,:]
+    result = np.zeros_like(array,dtype='float64')
+    result[2,:] = array[2,:] + voltage_bias
+    result[3,:] = array[3,:] + current_bias
+    result[4,:] = array[4,:] + temperature_bias
+    result[0:2,:] = time
+    return result
+
+print(f"{len(all_rw_cycles)} all_rw_cycles")
+
+expand_multiple = 60
+new_rw_cycles = []
+for rw_cycle in all_rw_cycles:
+    for i in range(expand_multiple-1):
+        new_rw_cycle = RW_cycle(rw_cycle.idx+1000*i)
+        new_rw_cycle.capacity = rw_cycle.capacity
+
+        noise_percent =     (random.random()-0.5)*0.02
+        voltage_bias =      (random.random()-0.5)*0.1
+        current_bias =      (random.random()-0.5)*0.005
+        temperature_bias =  (random.random()-0.5)*2.5
+        new_charge = add_noise(rw_cycle.charge,noise_percent)
+        new_charge = add_bias( new_charge,voltage_bias,current_bias,temperature_bias)
+        new_rw_cycle.charge = new_charge
+        try:
+            new_merged_discharge = add_noise(rw_cycle.merged_discharge,noise_percent)
+            new_merged_discharge = add_bias( new_merged_discharge,voltage_bias,current_bias,temperature_bias)
+            new_rw_cycle.merged_discharge = new_merged_discharge
+        except Exception as e:
+            print(f"{e} at RW_cycle {rw_cycle.__repr__()}")
+            break
+        new_rw_cycles.append(new_rw_cycle)
+all_rw_cycles = all_rw_cycles+new_rw_cycles
+
+print(f"{len(all_rw_cycles)} all_rw_cycles after expand")
 
 
 pass
